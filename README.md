ebea795c86fc3b7498b841a945d18fee4eea40b0
artsimboldo <rv.lange@gmail.com>
2019-01-16 22:08:19 -0500

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # maze
 maze generator in Go
+
+```
+prim := Prim{Width:5, Height:5, Seed:int64(0)}
+if _, err := prim.Generate(); err != nil {
+	fmt.Println(prim.String())
+}
+ _________
+|___   ___|
+|___   ___|
+| |_   ___|
+|_   ___  |
+|_______|_|
+```
--- maze_test.go
+++ maze_test.go
@@ -2,12 +2,14 @@ package maze
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestPrim(t *testing.T) {
 	prim0result := " _________\n|___   ___|\n|___   ___|\n| |_   ___|\n|_   ___  |\n|_______|_|\n"
 	prim := Prim{Width:5, Height:5, Seed:int64(0)}
 	if _, err := prim.Generate(); err == nil {
+		fmt.Println(prim.String())
 		if prim.String() != prim0result {
 			t.Fail()
 		}
