411248f5bd4a51d5a81f1d62b2eba28bc41740e6
artsimboldo <rv.lange@gmail.com>
2019-01-16 22:11:04 -0500

    update readme

--- maze_test.go
+++ maze_test.go
@@ -2,14 +2,12 @@ package maze
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestPrim(t *testing.T) {
 	prim0result := " _________\n|___   ___|\n|___   ___|\n| |_   ___|\n|_   ___  |\n|_______|_|\n"
 	prim := Prim{Width:5, Height:5, Seed:int64(0)}
 	if _, err := prim.Generate(); err == nil {
-		fmt.Println(prim.String())
 		if prim.String() != prim0result {
 			t.Fail()
 		}
